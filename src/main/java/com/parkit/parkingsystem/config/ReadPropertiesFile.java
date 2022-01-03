873595f1fce153698037842acaf36244041a464d
Quentin Schnurr <quentin.schnurr@gmail.com>
2022-01-03 10:57:24 +0100

    add properties

---
+++ src/main/java/com/parkit/parkingsystem/config/ReadPropertiesFile.java
@@ -0,0 +1,40 @@
+package com.parkit.parkingsystem.config;
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+
+public class ReadPropertiesFile {
+
+    private String username;
+    private String password;
+    private String url1;
+    private Properties properties;
+
+    public ReadPropertiesFile() throws IOException {
+
+        FileInputStream fis = new FileInputStream("src/main/resources/config.properties");
+
+        this.properties = new Properties();
+
+        properties.load(fis);
+
+        fis.close();
+    }
+
+    public String getUsername () {
+        return this.properties.getProperty("username");
+    }
+    public String getPassword () {
+        return this.properties.getProperty("password");
+    }
+    public String getUrl1 () {
+        return this.properties.getProperty("url1");
+    }
+    public String getUrl2 () {
+        return this.properties.getProperty("url2");
+    }
+}
